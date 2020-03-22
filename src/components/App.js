2b132bb2ff1c0ddf4f2e3e3a87150c38ae2b4cff
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-22 11:09:42 -0400

    Add LineChart

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/App.css';
+import LineChart from './LineChart';
 
 function App() {
   return (
     <svg width={800} height={800}>
-      {/* Insert components here */}
+      <LineChart height={200} width={300} />
     </svg>
   );
 }
--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,3 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as d3 from "d3"
 
+class LineChart extends Component {
+  state = { 
+    xScale: d3
+      .scaleLinear()
+      .domain([0, 1])
+      .range([0, this.props.width]),
+    yScale: d3
+      .scaleLinear()
+      .domain([0, 1])
+      .range([this.props.height, 0])
+  }
+  render() { 
+    return (  );
+  }
+}
+
+export default LineChart;
