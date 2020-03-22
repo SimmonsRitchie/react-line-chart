5197f347f70186ce166123db4caf5bb6889163ed
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-22 19:57:33 -0400

    Add button to generate random data

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,36 @@ import "../styles/App.css";
 import ResponsiveLineChart from "./LineChart";
 import Line from "./Line"
 import * as d3 from "d3";
-
 import Container from "./Container";
+
+const generateData = () => {
+  return d3.range(5).map(_ => {
+    return d3.range(20).map((_, idx) => ({ x: idx, y: Math.random() }));
+  })
+}
 
 // * Generates an array of arrays of {x, y} objects 
 // * Each represents a line for a multi-series line chart
-const data = d3.range(5).map(_ => {
-  return d3.range(20).map((_, idx) => ({ x: idx, y: Math.random() }));
-})
 
 class App extends Component {
+  state = {
+    data: generateData()
+  }
+
+  updateData = () => {
+    const data= generateData()
+    this.setState({data})
+  }
 
   render() {
 
     return (
       <Container>
         <ResponsiveLineChart
-          data={data}
+          data={this.state.data}
           dataline={({ d, idx }) => <Line d={d} seriesNo={idx} key={idx} />}
-          test={"hello?"}
         />
+        <button onClick={this.updateData}>Generate random data</button>
       </Container>
     );
   }
