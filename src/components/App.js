928f0c63b55bf48ae7099ccb708dc75f4e4d9b67
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-22 11:37:41 -0400

    Complete working line chart

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../styles/App.css';
 import LineChart from './LineChart';
+import * as d3 from 'd3'
+
+const data = d3.range(20).map((_, idx) => ({x: idx, y: Math.random()}));
 
 function App() {
+  console.log(data)
   return (
     <svg width={800} height={800}>
-      <LineChart height={200} width={300} />
+      <LineChart x={40} y={20} height={200} width={300} data={data}/>
     </svg>
   );
 }
--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,19 +1,44 @@
-import React, { Component } from 'react';
-import * as d3 from "d3"
+import React, { Component } from "react";
+import * as d3 from "d3";
+import Axis from "./Axis"
 
 class LineChart extends Component {
-  state = { 
+  state = {
     xScale: d3
       .scaleLinear()
-      .domain([0, 1])
+      .domain(d3.extent(this.props.data, d=>d.x))
       .range([0, this.props.width]),
     yScale: d3
       .scaleLinear()
       .domain([0, 1])
-      .range([this.props.height, 0])
+      .range([this.props.height, 0]),
+  };
+
+  valueLine = d3.line()
+    .x(d => this.state.xScale(d.x))
+    .y(d => this.state.yScale(d.y))
+
+  static getDerivedStateFromProps(props, state) {
+    let { xScale, yScale } = state;
+    xScale.range([0, props.width]);
+    yScale.range([props.height, 0]);
+    return { ...state, xScale, yScale };
   }
-  render() { 
-    return (  );
+
+  render() {
+    const { x, y, height, data } = this.props;    
+    const { xScale, yScale } = this.state;
+    console.log(this.valueLine(data))
+    return (
+      <g transform={`translate(${x}, ${y})`}>
+        <path 
+        d={this.valueLine(data)}     
+        style={{fill:'none',strokeWidth:2, stroke: "red"}}
+        />
+        <Axis x={0} y={0} type={"Left"} label="Y" scale={yScale} />
+        <Axis x={0} y={height} type={"Bottom"} label="x" scale={xScale} />
+      </g>
+    );
   }
 }
 
