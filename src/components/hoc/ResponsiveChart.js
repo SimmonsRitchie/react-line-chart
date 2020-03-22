2404fed263081ee60728a41d089e36f141fc65e1
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-22 17:35:48 -0400

    Create HOC

---
+++ src/components/hoc/ResponsiveChart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ResponsiveChart(ChartComponent) {
+    /*
+  * A HOC that takes a function a component and wraps it with an SVG and
+  * makes it responsive responsive container
+  *
+  * The D3render function returns what we want to render with d3
+  */
+}
