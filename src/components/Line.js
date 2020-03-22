a566d9601f3dd912697ad41d0c66abb2d59e61e5
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-22 19:40:42 -0400

    Give different color to each line

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import * as d3 from 'd3'
 
 const StyledLine = styled.path`
   fill: none;
   stroke-width: ${props => (props.highlight ? 5 : 2)};
-  stroke: ${props => props.customColor ? props.customColor : "red"};
+  stroke: ${props => props.strokeColor};
 `;
 
 class Line extends React.Component {
@@ -21,11 +22,13 @@ class Line extends React.Component {
   };
 
   render() {
-    const { d } = this.props;
+    const { d, seriesNo } = this.props;
     const { highlight} = this.state
+    const strokeColor = d3.schemeSet1[seriesNo]
     return <StyledLine 
       d={d} 
       highlight={highlight}
+      strokeColor={strokeColor}
       onMouseOver={this.highlight}
       onMouseOut={this.unhighlight}
       />;
